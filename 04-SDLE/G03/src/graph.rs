2f2083ce093e10445d7c255d2c91268b66f7d666
Marcelo Miranda <marcelo.caridade@protonmail.com>
2018-04-16 16:18:46 +0100

    Add G03

---
+++ 04-SDLE/G03/src/graph.rs
@@ -0,0 +1,88 @@
+use petgraph::{Graph, Undirected};
+use petgraph::graph::NodeIndex;
+use petgraph::algo::{is_cyclic_undirected, connected_components};
+use petgraph::dot::{Dot, Config};
+
+use rand::{self, Rng};
+use std::ops::{Index, IndexMut};
+
+pub enum GraphType {
+    Preferential,
+    Connected,
+}
+
+impl GraphType {
+    pub fn build(&self, nodes: usize) -> Graph<usize, (), Undirected> {
+        match *self {
+            GraphType::Preferential => build_preferential_graph(nodes),
+            GraphType::Connected => build_connected_graph(nodes),
+        }
+    }
+}
+
+pub fn build_connected_graph(nodes: usize) -> Graph<usize, (), Undirected> {
+    let mut graph = Graph::<usize, (), Undirected>::new_undirected();
+
+    for n in 0..nodes {
+        graph.add_node(n);
+    }
+
+    let indices: Vec<NodeIndex> = graph.node_indices().collect();
+    let mut rnd = rand::thread_rng();
+
+    while connected_components(&graph) > 1 {
+        let a = rnd.choose(&indices).unwrap();
+        let b = rnd.choose(&indices).unwrap();
+
+        graph.update_edge(*a, *b, ());
+    }
+
+    return graph;
+}
+
+fn select_by_degree(graph: &Graph<usize, (), Undirected>, mut selected: i32) -> NodeIndex {
+    for node_idx in graph.node_indices() {
+        let weight = graph.index(node_idx);
+        selected -= *weight as i32;
+
+        if selected <= 0 {
+            return node_idx;
+        }
+    }
+
+    unreachable!();
+}
+
+pub fn build_preferential_graph(nodes: usize) -> Graph<usize, (), Undirected> {
+    let mut graph = Graph::<_, (), Undirected>::new_undirected();
+
+    for _ in 0..nodes {
+        graph.add_node(1);
+    }
+
+    let indices: Vec<NodeIndex> = graph.node_indices().collect();
+    let mut rnd = rand::thread_rng();
+    let mut max = nodes;
+
+    while connected_components(&graph) > 1 {
+        let selected: i32 = rnd.gen_range(0, max as i32);
+        let a = rnd.choose(&indices).unwrap();
+        let b = select_by_degree(&graph, selected);
+
+        let edge = graph.update_edge(*a, b, ());
+
+        if *a == b || is_cyclic_undirected(&graph) {
+            graph.remove_edge(edge);
+        } else {
+            *graph.index_mut(*a) += 1;
+            *graph.index_mut(b) += 1;
+            max += 2;
+        }
+    }
+
+    return graph;
+}
+
+pub fn print_graph(graph: &Graph<usize, (), Undirected>) {
+    println!("{:?}", Dot::with_config(graph, &[Config::EdgeNoLabel]));
+}
---
+++ 04-SDLE/G03/src/main.rs
@@ -0,0 +1,195 @@
+#![feature(iterator_step_by)]
+
+extern crate petgraph;
+extern crate rand;
+extern crate clap;
+extern crate criterion_plot as plot;
+
+mod graph;
+
+use petgraph::{Graph, Undirected};
+use petgraph::graph::{Neighbors, NodeIndex};
+
+use rand::Rng;
+use std::ops::IndexMut;
+use std::str::FromStr;
+use clap::{App, Arg, SubCommand};
+use plot::prelude::*;
+use graph::*;
+
+const DEFAULT_NODES: usize = 20;
+const AVG_REPETITIONS: usize = 100;
+const STEPS: usize = 10;
+
+fn get_reliability(graph: &Graph<usize, (), Undirected>) -> f64 {
+    let nodes = graph.raw_nodes();
+    let receivers = nodes.iter().filter(|n| n.weight >= 1).count();
+    let total = nodes.len();
+
+    (receivers as f64 / total as f64)
+}
+
+fn average_reliability_for(graph: &Graph<usize,(),Undirected>, fraction: f32) -> f64 {
+    let mut acc = 0.0;
+
+    for _ in 0..AVG_REPETITIONS {
+        let bgraph = broadcast(&graph, fraction);
+        acc += get_reliability(&bgraph);
+    }
+
+    (acc / (AVG_REPETITIONS as f64))*100.0
+}
+
+fn plot_broadcast(nodes: usize, gtype: &GraphType) {
+    let ref xs: Vec<usize> = (10..nodes).step_by(STEPS).collect();
+    let gs: Vec<Graph<usize,(),Undirected>> = xs.iter().map(|x| { gtype.build(*x) }).collect();
+
+    Figure::new()
+		.configure(Axis::BottomX, |a| {
+			a.set(Label("Number of nodes"))
+			 .configure(Grid::Major, |g| g.show())
+		})
+		.configure(Axis::LeftY, |a| {
+			a.set(Label("Reliability"))
+			 .configure(Grid::Major, |g| g.show())
+		})
+        .configure(Key, |k| {
+            k.set(Position::Outside(Vertical::Top, Horizontal::Right))
+             .set(Order::SampleText)
+             .set(Title("Fraction"))
+        })
+		.plot(LinesPoints {
+			x: xs,
+            y: gs.iter().map(|g| average_reliability_for(g, 0.20))
+		}, |lp| {
+			lp.set(PointType::FilledCircle)
+			  .set(PointSize(0.6))
+              .set(Color::Cyan)
+              .set(Label("0.20"))
+		})
+		.plot(LinesPoints {
+			x: xs,
+            y: gs.iter().map(|g| average_reliability_for(g, 0.40))
+		}, |lp| {
+			lp.set(PointType::FilledCircle)
+			  .set(PointSize(0.6))
+              .set(Color::ForestGreen)
+              .set(Label("0.40"))
+		})
+		.plot(LinesPoints {
+			x: xs,
+            y: gs.iter().map(|g| average_reliability_for(g, 0.60))
+		}, |lp| {
+			lp.set(PointType::FilledCircle)
+			  .set(PointSize(0.6))
+              .set(Color::DarkViolet)
+              .set(Label("0.60"))
+		})
+		.plot(LinesPoints {
+			x: xs,
+            y: gs.iter().map(|g| average_reliability_for(g, 0.80))
+		}, |lp| {
+			lp.set(PointType::FilledCircle)
+			  .set(PointSize(0.6))
+              .set(Color::Magenta)
+              .set(Label("0.80"))
+		})
+		.plot(LinesPoints {
+			x: xs,
+            y: gs.iter().map(|g| average_reliability_for(g, 1.00))
+		}, |lp| {
+			lp.set(PointType::FilledCircle)
+			  .set(PointSize(0.6))
+              .set(Color::Red)
+              .set(Label("1.00"))
+		})
+		.draw()
+		.ok()
+		.and_then(|gnuplot| {
+			gnuplot.wait_with_output().ok().and_then(|p| String::from_utf8(p.stderr).ok())
+		});
+}
+
+fn sample_neighbors(neighbors: Neighbors<()>, fraction: f32) -> Vec<NodeIndex> {
+    let mut rnd = rand::thread_rng();
+
+    let n = fraction * neighbors.clone().count() as f32;
+    match rand::seq::sample_iter(&mut rnd, neighbors, n.ceil() as usize) {
+        Ok(neighbors) => neighbors,
+        Err(neighbors) => neighbors,
+    }
+}
+
+fn broadcast(graph: &Graph<usize, (), Undirected>, fraction: f32) -> Graph<usize, (), Undirected> {
+    let mut rand = rand::thread_rng();
+    let mut graph = graph.map(|_,_| { 0 }, |_,_| { () });
+    let mut to_visit = Vec::new();
+    let mut hops = 0;
+    let indices: Vec<NodeIndex> = graph.node_indices().collect();
+
+    to_visit.push(*rand.choose(&indices).unwrap());
+
+    while !to_visit.is_empty() {
+        hops += 1;
+        let mut neighbors = Vec::new();
+
+        for idx in to_visit.iter() {
+            {
+                let node = graph.index_mut(*idx);
+
+                if *node >= 1 {
+                    continue;
+                }
+
+                *node = hops;
+            }
+            let idx_neighbors = graph.neighbors(*idx);
+            neighbors.extend(sample_neighbors(idx_neighbors, fraction));
+        }
+        to_visit = neighbors;
+    }
+
+    return graph;
+}
+
+fn main() {
+    let app = App::new("probabilistic-broadcast")
+                          .about("Allows some sampling with probabilistic broadcast")
+                          .arg(Arg::with_name("Type")
+                               .help("Type of graph to generate")
+                               .default_value("connected"))
+                          .arg(Arg::with_name("N")
+                                .global(true)
+                                .help("Number of nodes"))
+                          .subcommand(SubCommand::with_name("generate")
+                                      .arg(Arg::with_name("Fraction")
+                                           .help("Fraction of neighbors to be selected")
+                                           .default_value("1.0")))
+                          .subcommand(SubCommand::with_name("plot"))
+                          .get_matches();
+
+    let nodes = app.value_of("N")
+        .and_then(|s| usize::from_str(s).ok())
+        .unwrap_or(DEFAULT_NODES);
+
+    let gtype = match app.value_of("Type") {
+        Some("preferential") => GraphType::Preferential,
+        _ => GraphType::Connected,
+    };
+
+    let fraction = match app.subcommand_matches("generate") {
+        None => 1.0,
+        Some(comm) => comm.value_of("Fraction")
+            .and_then(|s| f32::from_str(s).ok())
+            .unwrap(),
+    };
+
+    if app.is_present("plot") {
+        plot_broadcast(nodes+10, &gtype);
+    } else {
+        let graph = gtype.build(nodes);
+        let graph = broadcast(&graph, fraction);
+        print_graph(&graph);
+    }
+
+}
